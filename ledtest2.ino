be6daf0bcb77eb009386b5d114631c7d1da4de09
Andy Lemzikov <andylem@gmail.com>
2016-09-08 00:10:24 +0300

    Implement a simple motion detection

--- ledtest2.ino
+++ ledtest2.ino
@@ -3,6 +3,13 @@ int clock = 5; // SCK pin
 int latch = 6; // RCK pin
 int data = 7;  // DIO pin
 
+/*
+looking at the edge with contacts, having the sensor pointing to the front, the pins are from left to right: Ground, Signal, +5V
+*/
+int motionPin = 3;
+int lightPin = A0;
+int speakerPin = 11;
+
 #include "led8.h"
 
 Led8 led;
@@ -13,47 +20,40 @@ int dir = 1;
 
 void setup()
 {
+  pinMode(motionPin, INPUT);
+  pinMode(speakerPin, OUTPUT);
+  
   led.init(data, clock, latch);
   led.clear();
 
   led.moveToStart();
-  led.print(0);
-  led.print(1);
-  led.print(2);
-  led.print(3);
-  led.print(4);
-  led.print(5);
-  led.print(6);
-  led.print(7);
   led.clear();
   led.print(0, "A");
-  led.print(4, "B");
+  led.print(4, "L");
 }
 
 void loop()
 {
+  int m = digitalRead(motionPin);
+  int l = map(analogRead(lightPin), -1023, 1023, -99, 99);
   
-  led.moveToStart();
+  led.print(1, m, 2);
+  led.print(5, l, 3);
 
-  led.print(1, 12, 3);
-  led.print(5, -34, 3);
-  led.tick(0);
-//  int mils = millis();
-//  if (mils - lastmils < del)
-//    return;
-//  lastmils = mils;
-//
-//  led.moveToStart();
-//  for (int i = start; i < 8 + start; i++)
-//    led.print(String(i));
-//
-//  if (dir > 0)
-//    if (start < 8)
-//      start++;
-//    else
-//      dir = -1;
-//  else if (start > 0)
-//    start--;
-//  else
-//    dir = 1;
+  if (l > 60)
+  {
+    if (m == HIGH)
+    {
+      digitalWrite(speakerPin, HIGH);
+      delay(1);
+      digitalWrite(speakerPin, LOW);
+      delay(1);
+    }
+  }
+
+  for (int i=0;i<100;i++)
+  {
+    led.tick(0);
+    delay(1);
+  }
 }
